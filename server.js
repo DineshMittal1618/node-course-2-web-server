7600c6a08157c4f390780ee40e25bb14dbc5f2e7
Dinesh Mittal <Dinesh.Mittal@infostretch.com>
2019-06-17 10:56:59 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,75 @@
+const express=require('express');
+const hbs=require('hbs');
+var app=express();
+const fs=require('fs');
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerPartials(__dirname+'/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+ var log=`${now} ${req.method} ${req.url}`;
+
+fs.appendFile('server.log',log +'\n',(err)=>{
+  if(err){
+    console.log('unbale to append the appendFile');
+  }
+});
+  console.log(log)
+  next();
+});
+
+app.use((req,res,next)=>{
+  res.render('magnance.hbs',{
+    pageTitle:'magnance',
+
+  });
+});
+
+
+app.get('/',(req,res)=>{
+  res.render('home.hbs',{
+    pageTitle:'Home page',
+    currentYear:new Date().getFullYear()
+  });
+});
+
+
+// app.get('/',
+// (req,res)=>{
+// // res.send('<H1>Hello express</H1>');
+// res.send({
+//   name:'Dinesh',
+//   likes:['Swimming','Trecking']});
+// });
+
+
+// app.get('/about',
+// (req,res)=>{
+//   res.send('Hello About');
+// });
+
+
+app.get('/about',
+(req,res)=>{
+  res.render('about.hbs',{
+    pageTitle:'About page',
+    currentYear:new Date().getFullYear()
+  });
+}
+);
+app.get('/bad',
+(req,res)=>{
+  res.send({
+    ErrorMessage:"Bad request Made"});
+});
+app.listen(3000,()=>{console.log('Server Is ready to listen to port number 3000');});
